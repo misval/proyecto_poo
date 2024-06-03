2dcceb39304863517402e72c3ed9bb974318a253
Misael Valles <misaell.valles@gmail.com>
2024-06-03 15:26:01 -0300

    imagen circus y game platform

--- src/main/java/poo/games/GamePlatform.java
+++ src/main/java/poo/games/GamePlatform.java
@@ -5,8 +5,11 @@ import poo.games.pong.Pong;
 
 import java.awt.*;
 import java.awt.event.*;
-
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+
 
 import com.entropyinteractive.*; //las librerias JGame,GameLoop,KeyBoard,Mouse,etc...
 
@@ -30,6 +33,7 @@ public class GamePlatform extends JFrame implements ActionListener {
 		Panel menuVeticalIzquierdo = new Panel();
 		Panel navBar = new Panel();
 		Panel juegos = new Panel();
+		Panel containerJuegos = new Panel();
 //		ScrollPane juegos = new ScrollPane();
 
 		menuVeticalIzquierdo.setBackground(Color.GREEN);
@@ -39,7 +43,7 @@ public class GamePlatform extends JFrame implements ActionListener {
 //		CONTAINER PRINCIPAL
 		container.setLayout(new BorderLayout());
 		container.add(navBar, BorderLayout.NORTH);
-		container.add(juegos, BorderLayout.CENTER);
+		container.add(containerJuegos, BorderLayout.CENTER);
 		container.add(menuVeticalIzquierdo, BorderLayout.WEST);
 
 //		MENU VERTICAL IZQUIERDO
@@ -52,6 +56,9 @@ public class GamePlatform extends JFrame implements ActionListener {
 		Panel menuVerticalLabels = new Panel();
 		menuVerticalLabels.setLayout(new GridLayout(4, 1));
 		Panel emptyPanel = new Panel();
+		Panel emptyPanel2 = new Panel();
+		Button configuraciones = new Button("Configuraciones");
+		emptyPanel2.add(configuraciones);
 
 		menuVerticalLabels.add(epicGames);
 		menuVerticalLabels.add(homeLabel);
@@ -60,7 +67,7 @@ public class GamePlatform extends JFrame implements ActionListener {
 
 		menuVeticalIzquierdo.add(menuVerticalLabels, BorderLayout.NORTH);
 		menuVeticalIzquierdo.add(emptyPanel, BorderLayout.CENTER);
-		menuVeticalIzquierdo.add(emptyPanel, BorderLayout.SOUTH);
+		menuVeticalIzquierdo.add(emptyPanel2, BorderLayout.SOUTH);
 
 //		NAVBAR
 		navBar.setLayout(new BorderLayout());
@@ -77,23 +84,51 @@ public class GamePlatform extends JFrame implements ActionListener {
 
 		add(container);
 
-//		TODO: CREAR LAS CARDS PARA CADA JUEGO
-
+// JUEGOS PANELS
 		Panel juegosContainer = new Panel();
+		containerJuegos.setLayout(new BorderLayout());
+
+		Panel paddingSuperior = new Panel();
+		paddingSuperior.setPreferredSize(new Dimension(0, 20));
+		paddingSuperior.setBackground(Color.BLUE);
+		Panel paddingIzquierdo = new Panel();
+		paddingIzquierdo.setPreferredSize(new Dimension(20, 0));
+		paddingIzquierdo.setBackground(Color.BLUE);
+
 //		JUEGOS CONTAINER
-		juegos.setLayout(new GridLayout(1, 1));
-		juegosContainer.setLayout(new GridLayout(1, 4));
+		juegos.setLayout(new GridLayout(2, 4));
+		containerJuegos.add(paddingSuperior, BorderLayout.NORTH);
+		containerJuegos.add(juegos, BorderLayout.CENTER);
+		containerJuegos.add(paddingIzquierdo, BorderLayout.WEST);
+		juegosContainer.setLayout(new GridLayout(1, 4, 20, 10));
+
+		Panel auxPanel1 = new Panel();
+		Panel auxPanel2 = new Panel();
 
 //		JUEGO PANEL
 //		CIRCUS CHARLIE
 		Panel circusCharlie = new Panel();
 		circusCharlie.setLayout(new GridLayout(3, 1));
 
-		circusCharlie.setSize(new Dimension(40, 60));
+//		circusCharlie.setSize(new Dimension(40, 60));
 
-		Panel imageCircus = new Panel();
+//		BufferedImage imageC = ImageIO.read(getClass().getClassLoader().getResourceAsStream("imagenes/CircusCharlie_arcadeflyer.png"));
+		JLabel imageCircus = new JLabel();
 		Label titleCircus = new Label("Circus Charlie");
-		Button playCircus = new Button("Circus Charlie");
+
+//		panel de botones
+		Panel buttonsPanelC = new Panel();
+		buttonsPanelC.setLayout(new GridLayout(2, 2, 5, 0));
+		Panel auxPanelButtons2 = new Panel();
+		Panel auxPanelButtons1 = new Panel();
+
+		Button playCircus = new Button("Circus");
+		Button viewTableroCircus = new Button("Ver tablero");
+
+		buttonsPanelC.add(auxPanelButtons1);
+		buttonsPanelC.add(auxPanelButtons2);
+		buttonsPanelC.add(playCircus);
+		buttonsPanelC.add(viewTableroCircus);
 
 		playCircus.addActionListener(this);
 
@@ -101,18 +136,30 @@ public class GamePlatform extends JFrame implements ActionListener {
 
 		circusCharlie.add(imageCircus);
 		circusCharlie.add(titleCircus);
-		circusCharlie.add(playCircus);
+		circusCharlie.add(buttonsPanelC);
 
 //		PONG
 		Panel pong = new Panel();
 		pong.setLayout(new GridLayout(3, 1));
+		Panel buttonsPanelP = new Panel();
+		buttonsPanelP.setLayout(new GridLayout(2, 2, 5, 0));
+
+		Panel auxPanelButtons3 = new Panel();
+		Panel auxPanelButtons4 = new Panel();
+
+		buttonsPanelP.add(auxPanelButtons3);
+		buttonsPanelP.add(auxPanelButtons4);
+
 
 		pong.setSize(new Dimension(40, 60));
-
 
 		Panel imagePong = new Panel();
 		Label titlePong = new Label("Pong");
 		Button playPong = new Button("Pong");
+		Button viewTableroPong = new Button("Ver tablero");
+
+		buttonsPanelP.add(playPong);
+		buttonsPanelP.add(viewTableroPong);
 
 		playPong.addActionListener(this);
 
@@ -120,10 +167,12 @@ public class GamePlatform extends JFrame implements ActionListener {
 
 		pong.add(imagePong);
 		pong.add(titlePong);
-		pong.add(playPong);
+		pong.add(buttonsPanelP);
 
 		juegosContainer.add(circusCharlie);
 		juegosContainer.add(pong);
+		juegosContainer.add(auxPanel1);
+		juegosContainer.add(auxPanel2);
 		juegos.add(juegosContainer);
 
 
@@ -179,7 +228,7 @@ public class GamePlatform extends JFrame implements ActionListener {
 			t.start();
 		}
 
-		if (e.getActionCommand().equals("Circus Charlie")) {
+		if (e.getActionCommand().equals("Circus")) {
 			CircusCharlie juego = new CircusCharlie();
 
 			Thread t = new Thread() {
